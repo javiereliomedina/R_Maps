44a0b448185a216aec6cc1a184e69a7cb61b9545
Javier Elio <javiereliomedina@gmail.com>
2019-11-05 09:48:52 +0100

    Day 5

--- Rcode/30DayMapChallenge.R
+++ Rcode/30DayMapChallenge.R
@@ -48,3 +48,37 @@
   ggsave("Rresults/Day4_Hexagons_IB.png", width = 15, height = 15, units = "cm")  
   
   
+# Day 5: Raster ----
+## Radiometric data (Geological Survey, Ireland)
+## Download from https://www.gsi.ie/en-ie/programmes-and-projects/tellus/Pages/Data-and-Maps.aspx
+  
+  dir.create("Rdata")
+  Radiometric_URL <- "https://secure.dccae.gov.ie/GSI_DOWNLOAD/Geophysics/Data/GSI_Tellus_A5_RAD_GRIDS_2019.zip"
+  download.file(Radiometric_URL, destfile = "Rdata/Radiometrics.zip")
+  unzip(zipfile = "Rdata/Radiometrics.zip", exdir   = "Rdata/Radiometrics")
+  Uranium <- rgdal::readGDAL("Rdata/Radiometrics/A5_RAD_GRIDS_2019/GXF/A5_RAD_Uranium_equivalent.gxf")
+  # Color palette
+  Col_Ramp <- readxl::read_xls("Rdata/Radiometrics/A5_RAD_GRIDS_2019/Readme_Files_And_Instructions/QGIS_Clra_32_Geosoft_hex_Colour_Ramp.xls",
+                               sheet = 1,
+                               col_names = FALSE,
+                               skip = 4) %>%
+    select(3) %>%
+    as_vector() %>%
+    as.character()
+  
+  breaks <- quantile(Uranium@data$band1, probs = seq(0, 1, 1/38), na.rm = TRUE)
+  ggplot() + 
+    inlabru::gg(data = Uranium) + 
+    labs(title = "Airborne geophysical Gamma-Ray data",
+         subtitle = "GSI - Tellus 2019: A5 block (Limerick)") +
+    scale_fill_gradientn(name = "eU [ppm]",
+                         colors = Col_Ramp,
+                         values = scales::rescale(breaks),
+                         limits = c(min(breaks), max(breaks))) +
+    theme(legend.key.size = unit(1.5, "cm"),
+          legend.key.width = unit(0.5,"cm")) +
+    coord_fixed(ratio = 1)
+  
+  ggsave("Rresults/Day5_Raster_Tellus.png", height = 15, units = "cm")
+  unlink("Rdata", recursive = TRUE)
+  
